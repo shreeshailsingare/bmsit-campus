f57c701e05160b8b727fa49902e57de948133f49
shreeshailsingare <shreeshailsingare5@gmail.com>
2026-09-03 11:12:12 +0530

    Clean up HomePage.jsx by removing comments
    Removed commented-out code for HomePage component.

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,25 +1,3 @@
-// import React from "react";
-// import Event from "./Event";
-// import Posts from "./Posts";
-// import PostButton from '../CreatePost/PostButton.jsx';
-// import { useContext } from "react";
-// import { AuthContext } from "../SignUp/AuthContext";
-
-// function HomePage({ setFlash }) {
-//     const { user } = useContext(AuthContext);
-//     console.log("ROLE FROM UI:", user?.role);
-
-
-//     return(
-//        <>
-//        <Event/>
-//        <Posts currentUser={user} setFlash={setFlash}/>
-//        </>
-//     )
-// }
-
-// export default HomePage;
-
 import React from "react";
 import { Link } from "react-router-dom";
 import Event from "./Event";
