abc2d4a91f66418ed720eaa10166a1dee061aad7
shreeshailsingare <shreeshailsingare5@gmail.com>
2026-09-03 11:09:05 +0530

    Clean up main.jsx by removing commented code
    Removed commented-out code for imports and rendering.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { BrowserRouter } from "react-router-dom"; 
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-// import { AuthProvider } from './Components/SignUp/AuthContext.jsx';
-// import App from './App.jsx'
-// import { FlashProvider } from "./Context/FlashContext";
-// import axios from "axios";
-// axios.defaults.baseURL=import.meta.env.VITE_API_URL;
-// // axios.defaults.baseURL = "http://localhost:8080"
-// axios.defaults.withCredentials = true;
-
-// createRoot(document.getElementById('root')).render(
-//   <BrowserRouter>
-//    <FlashProvider>
-//     <AuthProvider>
-//       <App />
-//     </AuthProvider>
-//     </FlashProvider>
-//   </BrowserRouter>,
-// )
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"; 
